682c940113a84c7e331cad6da2904649a9b7b52b
Alan Choi <alan.choi@sendbird.com>
2021-12-12 13:29:09 +0900

    generate random messages

--- src/api.js
+++ src/api.js
@@ -1,11 +1,36 @@
 import { setupWorker, rest } from 'msw';
+import { loremIpsum } from 'lorem-ipsum';
+
+function getRandomText() {
+  return loremIpsum({
+    count: 1,
+    paragraphLowerBound: 3,
+    paragraphUpperBound: 7,
+    units: 'paragraph',
+  });
+}
+
+const allMessages = [...new Array(500)].map((_, index) => ({
+  id: index,
+  text: getRandomText(),
+}));
 
 export const worker = setupWorker(
   rest.get('/api/messages', (req, res, ctx) => {
+    const {
+      message_id = allMessages[allMessages.length - 1].id,
+      prev_limit = 20,
+    } = req.params;
+
+    const lastIndex = allMessages.findIndex(
+      (message) => message.id === message_id
+    );
+    const result = allMessages.slice(lastIndex - prev_limit, lastIndex);
+
     return res(
       ctx.delay(1500),
       ctx.status(200),
-      ctx.json({ messages: [{ id: 1, text: 'Hello' }] })
+      ctx.json({ messages: result })
     );
   })
 );
